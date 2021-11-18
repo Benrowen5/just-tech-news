256694d3492937ea9dd684a112b68fd85f2e849c
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-11-18 18:50:09 -0500

    logged in users can comment on and upvote posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -30,65 +30,65 @@ router.get('/', (req, res) => {
 });
 
 // GET a single post
-router.get('/:id', (req, res) => {
+router.get('/post/:id', (req, res) => {
     Post.findOne({
-        where: {
-            id: req.params.id
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
         },
-        attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id','comment_text','post_id','user_id','created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            },
-        ]
-    })
-    .then(dbPostData => {
-        if(!dbPostData) {
-            res.status(404).json({message: 'No post found with this id'});
-            return;
+        {
+          model: User,
+          attributes: ['username']
         }
-        res.json(dbPostData);
+      ]
     })
-    .catch(err => {
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+  
+        // pass data to template
+        res.render('single-post', { post });
+      })
+      .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    });
-});
-
-// POST a new post
-router.post('/', (req, res) => {
-    // expects {title: 'value', post_url: 'value', user_id: value}
-    Post.create({
-        title: req.body.title,
-        post_url: req.body.post_url,
-        user_id: req.body.user_id,
-    })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-        console.log(err);
-        res.stats(500).json(err);
-    });
-});
+      });
+  });
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-    // custom static method created in models.Post.js
-    Post.upvote(req.body, {Vote})
-        .then(updatedPostData => res.json(updatedPostData))
+    // make sure the session exists first
+    if (req.session) {
+      // pass session id along with all destructured properties on req.body
+      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
         .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
+          console.log(err);
+          res.status(500).json(err);
+          console.log('already voted on');
         });
-});
+    }
+  });
 
 // Update an existing post
 router.put('/:id', (req, res) => {
