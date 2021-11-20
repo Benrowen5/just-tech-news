1cc6a2e5ac9a54a675c1dd44641efd53ba423fd5
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-11-20 13:24:09 -0500

    added delete post functionality when in user dashboard - edit post

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const path = require('path');
-
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const session = require('express-session');
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 
 const sess = {
     secret: 'Super secret secret',
